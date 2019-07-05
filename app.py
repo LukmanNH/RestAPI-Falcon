a8dae3a70e8045ebac8749649c8e33e9837bdc7a
LukmanNH <luckmanhakim004@gmail.com>
2019-07-05 14:22:23 +0700

    Bug Fix

--- app.py
+++ app.py
@@ -12,29 +12,24 @@ class ObjRequestClass():
     def on_post(self , req, resp):
         data = json.loads(req.stream.read())
 
-        for i in u.find():
-            if data['username'] == i['username'] and data['password'] == i['password']:
-                resp.body = json.dumps({
-                    "code" : 200,
-                    "messages" : "Berhasil Login",
-                    "data" : {
-                        "username" : data["username"]
-                    }
-                })
-                print("Success")
-                resp.status = falcon.HTTP_200
-            else:   
-                resp.body = json.dumps({
-                    "code" : 401,
-                    "massages" : "Gagal Login-- username atau password salah"
-                })
-                resp.status = falcon.HTTP_401
-                print("Failed")
+        if 'username' in data.keys() and 'password' in data.keys():
+            username = data['username'][0]
+            password = data['password'][0]
+            resp.body = json.dumps({
+                "code" : 200,
+                "messages" : "Berhasil Login",
+                "data" : {
+                    "username" : data["username"]
+                }
+            })
+            resp.status = falcon.HTTP_200
+        else:   
+            resp.body = json.dumps({
+                "code" : 401,
+                "massages" : "Gagal Login-- username atau password salah"
+            })
+            resp.status = falcon.HTTP_401
 
-    # def on_post(self, req, resp):
-    #     posted_data = json.loads(req.stream.read())
-    #     print(str(type(posted_data)))
-    #     print(posted_data)
 
 
         
