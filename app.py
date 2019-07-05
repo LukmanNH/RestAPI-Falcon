620dac34b6141b8edfbd4a73d4deda5b221210f8
LukmanNH <luckmanhakim004@gmail.com>
2019-07-05 14:48:08 +0700

    bug fix querry db

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pymongo
 import json
 import falcon
 
-client = pymongo.MongoClient("mongodb+srv://luckman004:hackerisart1@cluster0-oacif.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://luckman004:hackerisart1@cluster0-oacif.mongodb.net/login?retryWrites=true&w=majority")
 db = client.login
 
 u = db.user
@@ -12,23 +12,26 @@ class ObjRequestClass():
     def on_post(self , req, resp):
         data = json.loads(req.stream.read())
 
-        if 'username' in data.keys() and 'password' in data.keys():
-            username = data['username'][0]
-            password = data['password'][0]
-            resp.body = json.dumps({
-                "code" : 200,
-                "messages" : "Berhasil Login",
-                "data" : {
-                    "username" : data["username"]
-                }
-            })
-            resp.status = falcon.HTTP_200
-        else:   
-            resp.body = json.dumps({
-                "code" : 401,
-                "massages" : "Gagal Login-- username atau password salah"
-            })
-            resp.status = falcon.HTTP_401
+        username = data['username']
+        password = data['password']
+        for i in u.find():
+            print(i)
+            if username in i['username'] and password in i['password']:
+                resp.body = json.dumps({
+                    "code" : 200,
+                    "messages" : "Berhasil Login",
+                    "data" : {
+                        "username" : data["username"],
+                        "password" : ""
+                    }
+                })
+                resp.status = falcon.HTTP_200
+            else:   
+                resp.body = json.dumps({
+                    "code" : 401,
+                    "massages" : "Gagal Login-- username atau password salah"
+                })
+                resp.status = falcon.HTTP_401
 
 
 
