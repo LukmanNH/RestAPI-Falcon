3ece94da30dde473fff4550820b7e73628139dcb
LukmanNH <luckmanhakim004@gmail.com>
2019-07-11 13:25:43 +0700

    reset password and new password

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ class Login(object):
         password = data['password']
         
         for i in u.find():
-            if username == i['username'] and password == i['password']:
+            if username == i['username'] and password == i['password']: 
                 resp.body = json.dumps({
                     "code" : 200,
                     "messages" : "Berhasil Login",
                     "data" : {
-                        "username" : data["username"]
+                        "username" : data["username"],
+                        "email" : i['email']
                     }
                 })
                 resp.status = falcon.HTTP_200
@@ -39,18 +40,20 @@ class Register():
 
         username = data['username']
         password = data['password']
+        email = data['email']
 
         for i in u.find():
-            if username == i['username']:
+            if username == i['username'] and email == i['email']:
                 resp.body = json.dumps({
                     "code" : 409,
-                    "massages" : "Gagal Register-- Username telah digunakan "
+                    "massages" : "Gagal Register-- Username atau email telah digunakan "
                     })
                 resp.status = falcon.HTTP_409
                 return 
         u.insert({
+            'email' : str(email),
             'username' : str(username),
-            'password' : str(password),
+            'password' : str(password)
         })
         resp.body = json.dumps({
                     "code" : 201,
@@ -60,7 +63,56 @@ class Register():
                     }
         })
         resp.status = falcon.HTTP_201
-        return 
+
+class ResetPassword():
+    def on_post(self,req,resp):
+        data  = json.loads(req.stream.read())
+
+        username = data['username']
+        email = data['email']
+    
+        reset_password = u.find_one_and_update({'username' : username,'email':email},{'$set':{'password':''}})
+        if reset_password != None:
+            resp.body = json.dumps({
+                "code": 200,
+                "message": 'Password berhasil di reset'
+            })
+            resp.status = falcon.HTTP_200
+        else:
+            resp.body = json.dumps({
+                "code" : 409,
+                "message" : 'Akun tidak terdaftar'
+            })
+            resp.status = falcon.HTTP_404
+
+
+class NewPassword():
+    def on_post(self,req,resp):
+
+        data = json.loads(req.stream.read())
+        
+        username = data['username']
+        email = data['email']
+        password = data['password']
+
+        new_password = u.find_one_and_update({'username': username, 'email' : email}, {'$set':{'password' : password}})
+        if new_password is True:
+            resp.body = json.dumps({
+                "code" : 409,
+                "message" : 'gagal di perbarui'
+            })
+            resp.status = falcon.HTTP_404
+            
+        else:
+            resp.body = json.dumps({
+                "code": 200,
+                "message": 'Password berhasil di perbarui'
+            })
+            resp.status = falcon.HTTP_200
+            
+
+
+
 
         
                 
@@ -69,6 +121,7 @@ class Register():
 api = falcon.API()
 api.add_route('/login', Login())
 api.add_route('/register', Register())
-# api.add_route('forgot', )
+api.add_route('/reset', ResetPassword())
+api.add_route('/new_pass' , NewPassword())
 
 
